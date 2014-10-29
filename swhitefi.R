91a16c4661e161f0421711afef1619156c7c931e
Shawn Whitefield <swhitefi@umich.edu>
2014-10-29 14:48:29 -0400

    first problem complete. tryig to figure out second one

--- swhitefi.R
+++ swhitefi.R
@@ -2,23 +2,35 @@
 #in the paper 
 #read in the words from the paper 
 #source your file in R
+#read in the file
 read.delim("mothur.txt")
-readPaper<-function(tf) {
-  words<-scan(tf,"") #this vector is made of characters
+##
+###
+#summary:
+#Given a file name, this function will return a list containing every word in the file
+readPaper<-function(file) {
+  text<-scan(file,"") #this vector is made of characters
   wordlist<-list()
-  for( i in 1:length(words)) {
-    wi<-words[i] #index of the word in the paper
-    wordlist[[wi]]<- c(wordlist[[wi]],i)
+  for(i in 1:length(text)) {
+    word<-text[i] #index of the word in the paper
+    wordlist[[word]] <- c(wordlist[[word]],i)
   }
-  return(wordlist)
+  return(wordlist) #return the list of words in the paper
 }
-mothurlist<-readPaper("mothur.txt")
-mothurlist
+#######################testing function
+mothurlist<-readPaper("mothur.txt") #this tests the list
+mothurlist # a test variable created to make sure the function worked
 #word count function
-#tell how many times a word occurs in the paper
-wordCount<-function(wordlist$word ) {
-  freqword<-sapply(wordlist$word,length)
-  return(freqword)
-}
+##############################
 
-wordCount(mothurlist)
+#tell how many times a word occurs in the paper
+#given a list containing the words in a file, tell how many times the word occurs
+wordCount<-function (wordlist, aWord) {
+  index.word<-(wordlist$aWord)) #store all locations of aWord
+  vec.index.word<-as.vector(index.word, mode="numeric") #store as a vector to extract elements
+  #return the length of the vector that has all occurences of word
+
+  return(length(vec.index.word))
+}
+#test code
+wordCount(mothurlist,improvement)
