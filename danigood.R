e1275795e3bff7ea02d5c344a2e9d659f74d9a71
Danielle G <danigood@umich.edu>
2014-10-31 10:32:20 -0400

    Getting started... writing pseudocode right now for readPaper

---
+++ danigood.R
@@ -0,0 +1,11 @@
+rm(list=ls())
+
+# Making the function readPaper. Currently intimidated by such a task. 
+# I'm going to look up scan and hopefully figure out what to do.
+# So I want to make a function named readPaper that you can input any text file 
+# name and the function will create a list of necessary info 
+# (which is? text? title perhaps?) Looking over the file and other commands I think
+# maybe just text? Let's jump in.
+readPaper <- function(x){
+	
+}
