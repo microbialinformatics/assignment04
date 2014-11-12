c05e48708d5ef025f8411863322db6c2bf1e9382
chianged <chianged@umich.edu>
2014-11-11 23:39:50 -0500

    Final edits

--- chianged.R
+++ chianged.R
@@ -10,7 +10,7 @@ readPaper <- function(file){
   text <- scan(file,"")               # Reads txt file to create a string of characters
   lower.text <- tolower(text)         # Makes everything lowercase
   text.list<- list(lower.text)        # Creates a list of the previous string of characters
-  return(text.list)
+  return(text.list)                   # Print output list
 }
 
 # References:
@@ -26,7 +26,7 @@ wordCount <- function(filelist, word){
   vector <- unlist(filelist)   # Convert filelist to vector
   TF <- vector == word         # Creates TRUE/FALSE vector
   total <- sum(TF)             # Sum TRUEs
-  return(total)
+  return(total)                # Print output vector
 }
 
 
@@ -38,7 +38,7 @@ wordCount <- function(filelist, word){
 wordPlacement <- function(filelist, word){
   vector <- unlist(filelist)      # Convert filelist to string
   nword <- which(word==vector)    # Where the word is found
-  return(nword)
+  return(nword)                   # Print output vector
 }
 
 # References: 
@@ -53,6 +53,7 @@ wordPlacement <- function(filelist, word){
 wordHist <- function(filelist, top=10){
  file.table <- table(filelist)                      # Calculate frequency of each unique word in text
  sort.table <- sort(file.table, decreasing=T)       # Sort from highest to lowest
+                                                    # Create barplot
  barplot(sort.table[1:top], width=1, space=0, names.arg=rownames(sort.table[1:top]), main = "Top Words", xlab="Word",ylab="Frequency")
  axis(1, labels=F, lwd.ticks=0)                     # Add x-axis w/o ticks
 }
@@ -74,7 +75,7 @@ nextWord <- function(filelist, word){
   leng <- length(vector)                     # Define length of filelist
   follow.words <- vector[position:leng]      # Creates vector from first occurance of word to end of filelist vector
   table <- table(follow.words)               # Calculate frequency of each word after target word
-  return(table)
+  return(table)                              # Print output vector
 }
 
 
@@ -89,7 +90,7 @@ previousWord <- function(filelist,word){
   position <- word.pos[1]                    # Take the first word location
   precede.words <- vector[1:position]        # Creates vector from beginning of filelist vector to first occurance of word
   table <- table(precede.words)              # Calculate frequency of each word preceding target word
-  return(table)
+  return(table)                              # Print output vector
 }
 
 
@@ -101,18 +102,19 @@ surpriseMe <- function(filelist){
   print("Are the authors of this article more eloquent* than Shakespeare?")
   print("(*measured by vocabulary size)")
   Shake.prop <- 5170/35000           # Shakespeare unique words / total words
-  vector <- unlist(filelist)         
-  table <- table(vector)             
+  vector <- unlist(filelist)         # Converts filelist to vector
+  table <- table(vector)             # Creates vector of unique words & their frequencies  
   unique <- nrow(table)              # Calculate total # of unique words
   total <- length(vector)            # Calculate total length of filelist
   Shake.adj <- Shake.prop * total    # Subsample Shakespeare to reflect article length
   matrix <- matrix(c(unique, Shake.adj), nrow=2, ncol=1)     # Make matrix to present results
-  colnames(matrix) <- "Unique words"
-  rownames(matrix) <- c("Article","Shakespeare")
-  print(matrix)
+  colnames(matrix) <- "Unique words"                         # Add column names to matrix
+  rownames(matrix) <- c("Article","Shakespeare")             # Add row names to matrix
+  print(matrix)                                              # Print matrix which shows the # of unique words in article vs. Shakespeare
+                                                             # Conclusion if article is more eloquent that Shakes, & vice versa
   conclusion <- ifelse (unique > Shake.adj, "Shakespeare ain't got nothin' on dem scientists", "The intellectuals doeth not compareth with the eloquence of Shakespeare")
-  print("Conclusion:")
-  return(conclusion)
+  print("Conclusion:")                                       # Format conclusion to be more clear
+  return(conclusion)                                         # Print conclusion
 }
 
 # References:
